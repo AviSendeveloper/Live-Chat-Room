7bac4ad9ee5b6b1c68875d4daabc567e2fa1e4d6
Avijit Sen <avijit.sen@tier5.in>
2023-08-24 18:54:48 +0530

    able to make connection between forntend and backend

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
+const socketIo = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+    console.log(`New client is connected with id: ${socket.id}`);
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
